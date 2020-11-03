c0615a2751557c5f3989094a573b22033811fa34
Ortwin Schneider <ortwin.schneider@googlemail.com>
2020-11-03 17:49:40 +0100

    add quarkus-rest to make OpenShift deployment work, remove consumer endpoint

--- src/main/java/com/redhat/os/ProducerRoute.java
+++ src/main/java/com/redhat/os/ProducerRoute.java
@@ -3,23 +3,25 @@ package com.redhat.os;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.amqp.AMQPComponent;
 
 @ApplicationScoped
 public class ProducerRoute extends RouteBuilder {
 
     @Inject
     Counter counter;
-
+    
     @Override
     public void configure() throws Exception {
+
+        // AMQPComponent amqp = getContext().getComponent("amqp", AMQPComponent.class);
         
         from("timer:foo?period=1000")
             .setBody(exchange -> "Incremented the counter: " + counter.increment())
+            .to("log:message-payload")
             .to("amqp:topic:counterTopic?clientId=amqp-quarkus-producer");
-                
-
-        from("amqp:topic:counterTopic")  
-        .to("log:example");      
+                      
     }
     
 }
+
